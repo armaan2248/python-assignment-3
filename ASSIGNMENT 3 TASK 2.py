f0d4ef7801237f41b1421c4abee7683d4b39525b
armaan2248 <armaanhshs@gmail.com>
2025-05-21 12:03:39 +0530

    Add files via upload

---
+++ ASSIGNMENT 3 TASK 2.py
@@ -0,0 +1,8 @@
+import math
+number = int(input("Enter a number: "))
+sqrt = math.sqrt(number)
+log = math.log(number)
+sine = math.sin(number)
+print("The sqrt of",number,"is", sqrt)
+print ("The log of",number, "is", log)
+print("The sine of",number,"is", sine)
